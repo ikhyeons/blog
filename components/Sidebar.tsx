4b5067af71bc255f48abbc5f39236fd9fc2e3022
ikhyeons <skantrkwl789@naver.com>
2023-06-17 19:16:53 +0900

    Style : 사이드바 CSS 구현 및 테스트 일부 수정

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,13 +2,29 @@ import styles from '@/styles/components/Sidebar.module.scss';
 
 function Sidebar() {
   return (
-    <nav className={styles.sidebar}>
-      <img
-        className={styles.img}
-        src="http://www.bizhankook.com/upload/bk/article/202306/thumb/25803-62395-sampleM.jpg"
-        alt=""
-      />
-    </nav>
+    <aside className={styles.sidebar}>
+      <div className={styles.profileBox}>
+        <img
+          className={styles.img}
+          src="http://www.bizhankook.com/upload/bk/article/202306/thumb/25803-62395-sampleM.jpg"
+          alt=""
+        />
+        <div className={styles.name}>잠자는오리</div>
+        <div className={styles.subName}>DrowzyDuck</div>
+        <span className={styles.visitor}>today 35</span>
+        <span className={styles.posting}>포스팅</span>
+      </div>
+      <div className={styles.ctg}>Category</div>
+      <ul className={styles.ctgList}>
+        <li>공지사항</li>
+      </ul>
+      <div className={styles.resume}>Resume</div>
+
+      <div className={styles.sidebarbottom}>
+        <div>Email : skantrkwl789@naver.com</div>
+        <div>GitHub : https://github.com/ikhyeons</div>
+      </div>
+    </aside>
   );
 }
 
--- test/Sidebar.test.tsx
+++ test/Sidebar.test.tsx
@@ -10,22 +10,30 @@ describe('Sidebar test', () => {
     expect(profile).toBeInTheDocument();
     // 프로필 닉네임이 있어야 함
     const name = screen.getByText('잠자는오리');
+    expect(name).toBeInTheDocument();
     // 서브 닉네임이 있어야 함
-    const subName = screen.getByText('잠자는오리');
-    // 총 방문자수, 금일 방문자수가 있어야 함
-    const visitor = screen.getByText('총 방문자 수');
-    const dayVisitor = screen.getByText('금일 방문자 수');
+    const subName = screen.getByText('DrowzyDuck');
+    expect(subName).toBeInTheDocument();
+    // 금일 방문자수가 있어야 함
+    const dayVisitor = screen.getByText('today 35');
+    expect(dayVisitor).toBeInTheDocument();
     // 포스팅 버튼이 있어야 함 / 클릭 시 글쓰기 페이지
     const postingBtn = screen.getByText('포스팅');
+    expect(postingBtn).toBeInTheDocument();
     // 카테고리가 있어야 함.
-    const Ctg = screen.getByText('카테고리');
+    const Ctg = screen.getByText('Category');
+    expect(Ctg).toBeInTheDocument();
     // 카테고리 내용이 있어야 함. / 클릭 시 해당 카테고리 글 목록
     const CtgTag = screen.getByText('공지사항');
+    expect(CtgTag).toBeInTheDocument();
     // 이력서 가는 네비게이션이 있어야 함 / 클릭 시 이력서 페이지
-    const resume = screen.getByText('resume');
+    const resume = screen.getByText('Resume');
+    expect(resume).toBeInTheDocument();
     // 이메일, 깃허브 텍스트 또는 버튼이 있어야 함.(버튼으로 할 듯)
-    const email = screen.getByText('Email');
-    const github = screen.getByText('GitHub');
+    const email = screen.getByText('Email : skantrkwl789@naver.com');
+    const github = screen.getByText('GitHub : https://github.com/ikhyeons');
+    expect(email).toBeInTheDocument();
+    expect(github).toBeInTheDocument();
     // 클릭 시 해당 페이지
   });
 });
