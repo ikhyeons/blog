04f9d19ed802865f5297dcdf2b5002ffe848e027
ikhyeons <skantrkwl789@naver.com>
2023-06-17 23:31:39 +0900

    Style : 사이드바 CSS 조정
    바텀영역은 더 수정해야 함.

--- components/sidebar/Category.tsx
+++ components/sidebar/Category.tsx
@@ -1,14 +1,25 @@
 import styles from '@/styles/components/Sidebar.module.scss';
+import { useState } from 'react';
 
 function Category() {
+  const ctgList = ctgListData();
   return (
     <>
       <div className={styles.ctg}>Category</div>
       <ul className={styles.ctgList}>
-        <li>공지사항</li>
+        {ctgList.map((data, i) => (
+          <li className={styles.ctgLi} key={i}>
+            {data}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 export default Category;
+
+export function ctgListData(): string[] {
+  const ctgList = ['공지', '리액트', '노드', '잡담'];
+  return ctgList;
+}
