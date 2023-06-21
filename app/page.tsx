0ce5c2d9ad3216f1dc5b442b4c4c8dc2ef59975f
ikhyeons <skantrkwl789@naver.com>
2023-06-22 02:46:26 +0900

    Refactor : 레이아웃 파일 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 import Index from '@/components/index/Index';
-import styles from '@/styles/app/index.module.scss';
 import Normal from '@/layouts/Normal';
+
 export default function Home() {
   return (
     <Normal>
-      <main className={styles.main}>
-        <Index />
-      </main>
+      <Index />
     </Normal>
   );
 }
