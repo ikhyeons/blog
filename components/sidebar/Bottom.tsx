43ca5bba578c6b99a0067570ad05ad59db53dcc3
ikhyeons <skantrkwl789@naver.com>
2023-06-22 03:13:39 +0900

    Style : 사이드바 하단 버튼 크기 수정

--- components/sidebar/Bottom.tsx
+++ components/sidebar/Bottom.tsx
@@ -5,10 +5,10 @@ function Bottom() {
     <div className={styles.sidebarBottom}>
       <div className={styles.linkBox}>
         <Link href={'https://github.com/ikhyeons'} target="_blank" className={styles.gitLink}>
-          GitHub : https://github.com/ikhyeons
+          <div className={styles.gitLink}>GitHub : https://github.com/ikhyeons</div>
         </Link>
         <Link href={'/resume'} target="_blank" className={styles.resumeLink}>
-          resume
+          <div className={styles.resumeLink}>resume</div>
         </Link>
       </div>
       <button className={styles.loginBtn}>로그인ㆍ가입</button>
