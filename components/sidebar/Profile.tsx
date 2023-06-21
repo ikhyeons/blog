2002b8938d358e5a66565cdc1f0757c4830b5d88
ikhyeons <skantrkwl789@naver.com>
2023-06-22 02:44:07 +0900

    Fix : 사이드바 프로필 이미지 로딩 에러 수정

--- components/sidebar/Profile.tsx
+++ components/sidebar/Profile.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/components/layout/Sidebar.module.scss';
 function Profile() {
   return (
     <div className={styles.profileBox}>
-      <img className={styles.img} src="duckProfile.jpg" alt="" />
+      <img className={styles.img} src="/duckProfile.jpg" alt="" />
       <div className={styles.name}>잠자는오리</div>
       <div className={styles.subName}>DrowzyDuck</div>
       <div className={styles.email}>skantrkwl789@naver.com</div>
