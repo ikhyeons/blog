b31f06010f484f35ab13e68900390a406909d61c
ikhyeons <skantrkwl789@naver.com>
2023-06-22 02:53:08 +0900

    Test : 글 목록 테스트 코드 추가

---
+++ test/ctg.test.tsx
@@ -0,0 +1,12 @@
+import SearchBar from '@/components/SearchBar';
+
+import { render, screen, fireEvent } from '@testing-library/react';
+
+describe('ctg test', () => {
+  it('is searchBar component rendered', () => {
+    render(<SearchBar />);
+
+    //검색바 인풋이 있어야 함
+    const searchBar = screen.getByPlaceholderText('Search');
+  });
+});
