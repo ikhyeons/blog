80a607bdfd25e0dfb294fc644912610c7138dcb3
ikhyeons <skantrkwl789@naver.com>
2023-06-27 10:46:06 +0900

    Feat : 글 보기 페이지 Toast ui 이용 글 뷰어 구현

--- components/view/Content.tsx
+++ components/view/Content.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+'use client';
+
 import styles from '@/styles/components/view/view.module.scss';
 
+import '@toast-ui/editor/dist/toastui-editor-viewer.css';
+import './toastui-css-wrapper.css';
+import 'highlight.js/styles/tokyo-night-dark.css';
+
+import { Viewer } from '@toast-ui/react-editor';
+
 function Content() {
-  return <article className={styles.content}>Content</article>;
+  const contentData = view();
+
+  return (
+    <article className={styles.mainContent}>
+      <Viewer initialValue={contentData} />
+    </article>
+  );
 }
 
 export default Content;
+
+function view() {
+  return `<h3>기존에 Recoil로 만들었던 프로젝트를 Redux로 마이그레이션 하였다.</h3><p><br></p>`;
+}
