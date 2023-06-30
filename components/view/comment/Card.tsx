2b2d511b72398205bce7efddb10e55d85ed0b55f
ikhyeons <skantrkwl789@naver.com>
2023-07-01 01:00:19 +0900

    Feat : 댓글 좋아요 이모티콘 추가

--- components/view/comment/Card.tsx
+++ components/view/comment/Card.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '@/styles/components/view/view.module.scss';
+import { FiThumbsUp } from 'react-icons/fi';
 
 function Card({ indent }: { indent: boolean }) {
   return (
@@ -8,7 +9,9 @@ function Card({ indent }: { indent: boolean }) {
         <span style={{ marginRight: '10px' }}>슬픈개미핥기</span>
         <span style={{ marginRight: '10px' }}>2023.06.13</span>
         <span style={{ marginRight: '10px' }}>답글</span>
-        <span>b 17</span>
+        <span>
+          <FiThumbsUp /> 17
+        </span>
       </p>
       <p className={styles.content}>그리고 잠시 사이를 두어</p>
     </li>
