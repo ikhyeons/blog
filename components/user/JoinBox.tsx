643151afa4e5c85600d1b4b4220e10f20f2ff747
ikhyeons <skantrkwl789@naver.com>
2023-07-03 23:40:37 +0900

    Feat : 회원가입 페이지 키다운 시 이벤트 구현

--- components/user/JoinBox.tsx
+++ components/user/JoinBox.tsx
@@ -1,9 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from '@/styles/components/user/User.module.scss';
 
 function JoinBox({ setType }: { setType: React.Dispatch<React.SetStateAction<'main' | 'login' | 'join'>> }) {
   const [step, setStep] = useState(0);
   const [validate, setValidate] = useState(false);
+  const nickNameInputRef = useRef<HTMLInputElement>(null);
+  const emailInputRef = useRef<HTMLInputElement>(null);
+  const pwInputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     setStep(1);
@@ -25,6 +28,7 @@ function JoinBox({ setType }: { setType: React.Dispatch<React.SetStateAction<'ma
       <div className={`${styles.inputWrap} ${step >= 1 ? styles.onAct : null}`}>
         NickName
         <input
+          ref={nickNameInputRef}
           role="nickNameInput"
           spellCheck="false"
           onChange={() => {
@@ -33,6 +37,9 @@ function JoinBox({ setType }: { setType: React.Dispatch<React.SetStateAction<'ma
           onKeyUp={(e) => {
             if (e.key === 'Enter') {
               setStep(2);
+              setTimeout(() => {
+                emailInputRef.current?.focus();
+              }, 20);
             }
           }}
           type="text"
@@ -41,12 +48,31 @@ function JoinBox({ setType }: { setType: React.Dispatch<React.SetStateAction<'ma
 
       <div className={`${styles.inputWrap} ${step == 2 ? styles.onAct : null}`}>
         Email
-        <input role="emailInput" type="email" />
+        <input
+          onKeyUp={(e) => {
+            if (e.key === 'Enter') {
+              setStep(2);
+              pwInputRef.current?.focus();
+            }
+          }}
+          ref={emailInputRef}
+          role="emailInput"
+          type="email"
+        />
       </div>
 
       <div className={`${styles.inputWrap} ${step == 2 ? styles.onAct : null}`}>
         Password
-        <input type="password" />
+        <input
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              alert('join action');
+            }
+          }}
+          ref={pwInputRef}
+          type="password"
+        />
+        {'  '}ㄴEnter로 회원가입
       </div>
     </>
   );
