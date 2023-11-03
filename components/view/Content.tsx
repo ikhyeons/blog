3242b9fef81079cc409cec910826d1464de094bf
ikhyeons <skantrkwl789@naver.com>
2023-11-03 15:54:22 +0900

    Error : 하이라이트 미적용 수정

--- components/view/Content.tsx
+++ components/view/Content.tsx
@@ -1,22 +1,18 @@
-'use client';
-
 import styles from '@/styles/components/view/view.module.scss';
 import styles2 from '@/styles/components/view/content.module.scss';
 
 import hljs from 'highlight.js/lib/core';
 import javascript from 'highlight.js/lib/languages/javascript';
-import { useEffect } from 'react';
 import 'highlight.js/styles/github-dark.css';
+
 hljs.registerLanguage('javascript', javascript);
 
 function Content({ data }: { data: string }) {
-  useEffect(() => {
-    hljs.highlightAll();
-  }, []);
+  const highlightedCode = hljs.highlight(data, { language: 'javascript' }).value;
 
   return (
     <article className={styles.mainContent}>
-      <div className={styles2.innerContent} dangerouslySetInnerHTML={{ __html: data }}></div>
+      <div className={styles2.innerContent} dangerouslySetInnerHTML={{ __html: highlightedCode }}></div>
     </article>
   );
 }
