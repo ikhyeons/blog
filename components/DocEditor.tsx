ed007b6b6fe07a8964debcc7c8cb679e2493e36a
ikhyeons <skantrkwl789@naver.com>
2023-11-07 15:11:40 +0900

    Update : 글 에디터 글자색상 플러그인 추가

--- components/DocEditor.tsx
+++ components/DocEditor.tsx
@@ -5,6 +5,10 @@ import '@toast-ui/editor-plugin-code-syntax-highlight/dist/toastui-editor-plugin
 import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
 import { useAppDispatch, useAppSelector } from '@/utils/hooks/redux';
 import { updateContent } from '@/utils/redux/reducer/docFormSlice';
+import 'tui-color-picker/dist/tui-color-picker.css';
+import '@toast-ui/editor-plugin-color-syntax/dist/toastui-editor-plugin-color-syntax.css';
+
+import colorSyntax from '@toast-ui/editor-plugin-color-syntax';
 
 import { Prism } from 'prism-react-renderer';
 function DocEditor() {
@@ -14,10 +18,11 @@ function DocEditor() {
   return (
     <Editor
       ref={editorRef}
-      plugins={[[codeSyntaxHighlight, { highlighter: Prism }]]}
+      plugins={[[codeSyntaxHighlight, { highlighter: Prism }], colorSyntax]}
       initialValue={`${content}`}
       initialEditType="wysiwyg"
       theme={'dark'}
+      height="70vh"
       onChange={() => {
         dispatch(updateContent(editorRef.current?.getInstance().getHTML()!));
       }}
