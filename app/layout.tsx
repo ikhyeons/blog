13a3baa70141be7e8e6ef5180823934c6f0f87d8
ikhyeons <skantrkwl789@naver.com>
2024-01-06 02:56:59 +0900

    Update : 폰트 가져오는 위치 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import ModalWrap from '@/components/modal/ModalWrap';
 import './globals.css';
 import { Providers } from '@/utils/redux/Providers';
 import NextTopLoader from 'nextjs-toploader';
+import Head from 'next/head';
+import Link from 'next/link';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
