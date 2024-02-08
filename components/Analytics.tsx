232b195deb4a8a6e34031bd1836c59c2b874be8b
ikhyeons <skantrkwl789@naver.com>
2024-02-09 04:10:25 +0900

    Update : GA 적용

--- components/Analytics.tsx
+++ components/Analytics.tsx
@@ -3,22 +3,15 @@ import Script from 'next/script';
 function Analytics() {
   return (
     <>
-      {
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-${process.env.GTM_ID}"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
-      }
+      <Script id="tgs" async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GTM_ID}`}></Script>
+      <Script id="tgsm">
+        {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-      <Script id="gtm">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-${process.env.GTM_ID}');`}</Script>
+  gtag('config', '${process.env.GTM_ID}');`}
+      </Script>
     </>
   );
 }
