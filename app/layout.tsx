9de8ee342176b5db070e21dcb36ded0f95e6df05
ikhyeons <skantrkwl789@naver.com>
2024-02-09 03:40:35 +0900

    Update : GA 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,14 @@ import ModalWrap from '@/components/modal/ModalWrap';
 import './globals.css';
 import { Providers } from '@/utils/redux/Providers';
 import NextTopLoader from 'nextjs-toploader';
+import Analytics from '@/components/Analytics';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
       <Providers>
         <body style={{ background: 'var(--bg-main)' }}>
+          <Analytics />
           <NextTopLoader
             color="#bb00aa"
             showSpinner={true}
---
+++ components/Analytics.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Script from 'next/script';
+function Analytics() {
+  return (
+    <>
+      {`<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-${process.env.GTM_ID}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->`}
+
+      <Script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-${process.env.GTM_ID}');`}</Script>
+    </>
+  );
+}
+
+export default Analytics;
